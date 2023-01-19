e8301789f8c8bf172591c1eff609e4aeb58addfb
Sabrina Momotaz Neha <80744932+SABRINAa2@users.noreply.github.com>
2023-01-19 10:27:04 +0600

    Add files via upload

---
+++ C#/Non primitive data types/Non primitive data types/Program.cs
@@ -0,0 +1,43 @@
+﻿using Non_primitive_data_types;
+using Non_primitive_data_types.Gender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Non_primitive_data_types
+{
+    public class Student
+    {
+
+        public int Id;
+        public string Name;
+        public int Age;
+
+        public void Identity()
+        {
+
+            Console.WriteLine(Id);
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.ReadLine();
+
+        }
+    }
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Female = new Student();
+            Female.Id =  44817;
+            Female.Name = "Neha";
+            Female.Age = 21;
+            Female.Identity();
+
+            Non_primitive_data_types.Gender.University.add
+
+
+    }
+}
---
+++ C#/PrimitiveTypes/PrimitiveTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringVar = "Hello World!!";
+            int intVar = 100;
+            float floatVar = 10.2f;
+            char charVar = 'A';
+            bool boolVar = true;
+
+            var firstName = "Sabrina Momotaz";
+            var lastName = "Neha";
+         
+
+            Console.WriteLine(" First Name : " + firstName);
+            Console.WriteLine("Last Name : " + lastName);
+            Console.WriteLine("Full Name : ' + firstName + " " +lastName");
+            Console.ReadLine();
+
+            const float Pi = 3.1416f;
+            var age = 22;
+        }
+    }
+}
