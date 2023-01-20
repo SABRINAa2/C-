47c778f468e54bf00491f58c69b58fae193583da
Sabrina Momotaz Neha <80744932+SABRINAa2@users.noreply.github.com>
2023-01-20 09:11:19 +0600

    Add files via upload

---
+++ C#/Practise1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+                Console.WriteLine("\n\n\n\tFahim Rahman");
+
+
+
+                string firstName = "Sabrina";
+                string lastName = "Fahim";
+                Console.WriteLine("\n\n" + firstName + " + " + lastName);
+
+
+
+                int x = 10;
+                Console.WriteLine("The value of x is : " + x);
+
+
+
+
+                int y = 5;
+                Console.WriteLine("The value of y is : " + y);
+
+
+
+                int z = x + y;
+                int r = x - y;
+                int s = x * y;
+                double d = x / y;
+
+
+
+
+                Console.WriteLine("The summation of x and y is : " + z);
+                Console.WriteLine("The subtraction of x and y is : " + r);
+                Console.WriteLine("The multiplication of x and y is : " + s);
+                Console.WriteLine("The division of x and y is : " + d);
+
+
+
+            }
+        }
+    }
+}
+    }
+}
