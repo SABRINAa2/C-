c043b941b759b3acb17f7ba91ceafc4e70467c58
Sabrina Momotaz Neha <80744932+SABRINAa2@users.noreply.github.com>
2023-01-20 11:00:45 +0600

    Add files via upload

---
+++ C#/Array/Array/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "BMW", "VOLBO", "FORD" };
+
+
+
+            //Index change
+
+
+
+            //Change index
+            cars[2] = "Toyota";
+
+
+
+            Console.WriteLine("After Update cars:" + cars[0] + " " + cars[1] + " " + cars[2]);
+
+
+
+
+
+
+            int[] numbers = { 1, 2, 3 };
+
+
+
+            // Console.WriteLine("Cars :" + cars[0] +" "+ cars[1]+" " + cars[2]);
+            Console.WriteLine("Numbers : " + numbers[0] + " " + numbers[1] + " " + numbers[2]);
+
+
+
+
+
+
+
+
+            //NON PRIMITIVE: STRING
+
+
+
+
+            string firstName = "Alex";
+            string lastName = "Doolean";
+
+
+
+            string fullName = string.Concat(firstName, lastName);
+            Console.WriteLine("The full Name is :" + fullName);
+
+
+
+            //Check index of a letter
+
+
+
+            Console.WriteLine("The number of index of x is : " + firstName.IndexOf("x"));
+
+
+
+            //Check letter of a index
+            Console.WriteLine("The 3rd index value is : " + firstName[3]);
+
+
+
+
+
+        }
+    }
+
+}
---
+++ C#/Practise/Practise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace ConditionalStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //if - else statement
+            int number = 10;
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is Even");
+            }
+
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+
+            int age = 58;
+
+            if (age <= 17)
+            {
+                Console.WriteLine("Kid");
+            }
+            else if (age >= 18 && age < 50)
+            {
+                Console.WriteLine("Young");
+            }
+            else { Console.WriteLine("Old"); }
+
+            //conditional operator
+        }
+    }
+}
+
