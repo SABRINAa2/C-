c6cda3b87768222ae0fdaa90d607b25410e4aa18
Sabrina Momotaz Neha <80744932+SABRINAa2@users.noreply.github.com>
2023-02-02 18:33:15 +0600

    Add files via upload

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x = 10.5, y = 10.5;
+            int number = 15, h = 5, k = 10;
+
+
+            Console.WriteLine(x + y);
+            char a= 'B';
+             string b = "Neha";
+            string p = "Fahim";
+            Console.WriteLine( b+"+" +p);
+             bool c = true;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(a);
+            Console.WriteLine("My name is\n"+b);
+            Console.WriteLine(c);
+            System.Console.ReadLine();
+        }
+    }
+}
