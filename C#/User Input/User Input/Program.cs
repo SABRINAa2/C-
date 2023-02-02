6121b83e5a0d0e7da80fd0c74de6cb0279708381
Sabrina Momotaz Neha <80744932+SABRINAa2@users.noreply.github.com>
2023-02-02 18:32:25 +0600

    Add files via upload

---
+++ C#/User Input/User Input/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name :");
+
+            string myName = Console.ReadLine();
+            Console.WriteLine("You have entered your name  " + myName);
+            Console.ReadLine();
+            Console.WriteLine("Enter the value of x : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the summation of x and y is : " + (x + y));
+            Console.WriteLine(the subtraction of x and y is : " +(x-y));
+
+            
+            
+            
+            
+            Console.ReadLine();
+        }
+
+
+    }
+}
