0906514d8ad2cbcbfba9dded91824d2c2a71073d
Sabrina Momotaz Neha <80744932+SABRINAa2@users.noreply.github.com>
2023-02-02 18:31:26 +0600

    Add files via upload

---
+++ C#/Break continue/Break continue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = 15;
+            Console.WriteLine(number);
+            double list = number;
+            Console.WriteLine(list);
+
+            long x = 10;
+            int y =(int) x;
+            Console.WriteLine(x);
+
+            double d = 10.5;
+            int i = (int)d;
+            Console.WriteLine(i);
+            int myInt = 10;
+            Console.WriteLine(Convert.ToString("Hello"+myInt));
+            string str = "Momotaz";
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
